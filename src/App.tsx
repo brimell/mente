f33169f1658513d9542c62d09cd5a94f6ccd36ee
Raaydon <billjrimell@gmail.com>
2024-02-17 22:13:47 +0000

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React, { useContext, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Updated import
 import MainContextProvider, { MainContext } from "./contexts/MainContext";
 import LoginPage from "./components/LoginPage";
-
-import { Layout} from "antd";
-
+import { Layout } from "antd";
 import AppHeader from "./components/AppHeader";
 
 const { Content } = Layout;
 
 const HomePage: React.FC = () => {
 	const { currentUser } = useContext(MainContext);
-  console.log(currentUser);
+	console.log(currentUser);
 
 	return (
 		<MainContextProvider>
 			<Suspense fallback={<div>Loading...</div>}>
-				<Layout>
-					<AppHeader />
-					<Content style={{ marginTop: 64 }}>
-						{!currentUser ? <LoginPage /> : null}
-					</Content>
-				</Layout>
+				<Router>
+					<Layout>
+						<AppHeader />
+						<Content style={{ marginTop: 64 }}>
+							<Routes>
+								<Route path="/login" element={<LoginPage />} />
+								<Route
+									path="/"
+									element={
+										!currentUser ? <LoginPage /> : null
+									}
+								/>
+							</Routes>
+						</Content>
+					</Layout>
+				</Router>
 			</Suspense>
 		</MainContextProvider>
 	);
