a9ccc868252b4d9652a0baad62a466b055c616dd
Raaydon <billjrimell@gmail.com>
2024-02-17 16:33:57 +0000

    fixed firebaseContext

--- app/contexts/FirebaseContext.tsx
+++ app/contexts/FirebaseContext.tsx
@@ -1,19 +1,30 @@
-"use client"
-import React, { createContext, useContext } from "react";
-import firebase from "firebase/app";
-import { app } from "../firebaseConfig";
+"use client";
+import React, { createContext } from "react";
+import { initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
 
-export const FirebaseContext = createContext<firebase.firestore.Firestore | null>(
-  null
-);
+interface FirebaseContextType {
+  db: Firestore;
+}
 
+export const FirebaseContext = createContext<FirebaseContextType | null>(null);
 
-const FirebaseProvider: React.FC = ({ children }) => {
-  // Get the Firestore instance from Firebase
-  const db = firebase.firestore();
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
+const app = initializeApp(firebaseConfig);
+const db: Firestore = getFirestore(app);
+
+const FirebaseProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <FirebaseContext.Provider value={db}>
+    <FirebaseContext.Provider value={{db}}>
       {children}
     </FirebaseContext.Provider>
   );
