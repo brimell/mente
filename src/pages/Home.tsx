f72fd67988f44cf71fd54e3ca33a0b1177676c72
Raaydon <billjrimell@gmail.com>
2024-02-18 00:36:49 +0000

    working animations now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,12 @@
 import { useState } from "react";
 import { Layout, Card, Form } from "antd";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faLaugh,
-	faFrown,
-	faAngry,
-	faSmile,
-	faMeh,
-} from "@fortawesome/free-solid-svg-icons";
+
+import Moods from "../components/Moods";
 
 const { Content } = Layout;
 
 const HomePage = () => {
 	const [moodData, setMoodData] = useState([]);
-
-	const addMood = (mood) => {
-		// Add mood data to state
-		setMoodData([...moodData, { mood, date: new Date().toISOString() }]);
-	};
 
 	return (
 		<>
@@ -33,7 +22,7 @@ const HomePage = () => {
 				))}
 			</div>
 
-			<Card title="Add Mood" bordered={false} style={{ width: 300 }}>
+			<Card title="Add Mood" bordered={false} style={{ width: "fit-content" }}>
 				<Form name="add-mood-form">
 					<div
 						style={{
@@ -41,31 +30,7 @@ const HomePage = () => {
 							justifyContent: "space-between",
 						}}
 					>
-						<FontAwesomeIcon
-							icon={faAngry}
-							style={{ fontSize: "24px", cursor: "pointer" }}
-							onClick={() => addMood(1)}
-						/>
-						<FontAwesomeIcon
-							icon={faFrown}
-							style={{ fontSize: "24px", cursor: "pointer" }}
-							onClick={() => addMood(2)}
-						/>
-						<FontAwesomeIcon
-							icon={faMeh}
-							style={{ fontSize: "24px", cursor: "pointer" }}
-							onClick={() => addMood(3)}
-						/>
-						<FontAwesomeIcon
-							icon={faSmile}
-							style={{ fontSize: "24px", cursor: "pointer" }}
-							onClick={() => addMood(4)}
-						/>
-						<FontAwesomeIcon
-							icon={faLaugh}
-							style={{ fontSize: "24px", cursor: "pointer" }}
-							onClick={() => addMood(5)}
-						/>
+						<Moods />
 					</div>
 				</Form>
 			</Card>
