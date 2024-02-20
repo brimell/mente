4a146a5089d4742929fb29b79c8f3a4abe55d996
Raaydon <billjrimell@gmail.com>
2024-02-20 00:43:00 +0000

    fix notification location

--- src/components/mood-recorder.jsx
+++ src/components/mood-recorder.jsx
@@ -4,12 +4,12 @@ import { Box, Card, Typography, Snackbar, Alert } from '@mui/material';
 import Moods from 'src/components/moods';
 import { MainContext } from '../contexts/MainContext';
 import { addMoodToFirestore, getLastMoodSubmission } from 'src/utils/firestore';
-import useNotification from "../hooks/useNotification"; // Make sure this path is correct
+import useNotification from '../hooks/useNotification'; // Make sure this path is correct
 
 export default function MoodRecorder() {
   const { currentUser } = useContext(MainContext);
   const { open, message, severity, triggerNotification, handleClose } = useNotification();
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState('');
 
   useEffect(() => {
     const checkLastSubmitted = async () => {
@@ -21,9 +21,12 @@ export default function MoodRecorder() {
         if (hoursDiff < 1) {
           setSelected(result.mood);
           // Optionally, trigger a notification about the cooldown
-          triggerNotification('You have recently submitted a mood. Please wait to submit another.', 'info');
+          triggerNotification(
+            'You have recently submitted a mood. Please wait to submit another.',
+            'info'
+          );
         } else {
-          setSelected(""); // Reset selected if it's been more than an hour
+          setSelected(''); // Reset selected if it's been more than an hour
         }
       }
     };
@@ -32,24 +35,42 @@ export default function MoodRecorder() {
   }, [currentUser]);
 
   const handleMoodSubmit = async (mood) => {
-    await addMoodToFirestore(mood, currentUser, () => {
-      triggerNotification('Mood set successfully!', 'success');
-      setSelected(mood); // Update the selected mood
-    }, () => {
-      triggerNotification('Failed to set mood. Try again.', 'error');
-    });
+    await addMoodToFirestore(
+      mood,
+      currentUser,
+      () => {
+        triggerNotification('Mood set successfully!', 'success');
+        setSelected(mood); // Update the selected mood
+      },
+      () => {
+        triggerNotification('Failed to set mood. Try again.', 'error');
+      }
+    );
   };
 
   return (
     <>
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        style={{ top: '65px' }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
         <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
           {message}
         </Alert>
       </Snackbar>
-      <Card style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+      <Card
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+        }}
+      >
         <Typography variant="h6" sx={{ mb: 5 }} style={{ margin: '0' }}>
-          {selected ? "Current mood" : "Record your mood!"}
+          {selected ? 'Current mood' : 'Record your mood!'}
         </Typography>
         <Box sx={{ p: 5 }}>
           <Moods handleMoodSubmit={handleMoodSubmit} selected={selected} />
