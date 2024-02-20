cfd0610db2c325fa5f64d8dd0523b9365dd05e9a
Raaydon <billjrimell@gmail.com>
2024-02-20 12:25:19 +0000

    fix sizing

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -1,31 +1,37 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTableColumns } from '@fortawesome/free-solid-svg-icons';
+import {
+  faTableColumns,
+  faFaceLaughWink,
+  faBed,
+  faPersonRunning,
+  faMagnifyingGlassChart,
+} from '@fortawesome/free-solid-svg-icons';
 
 const navConfig = [
   {
     title: 'overview',
     path: '/app',
-    icon: <FontAwesomeIcon icon={faTableColumns} />,
+    icon: <FontAwesomeIcon icon={faTableColumns} size='lg' />,
   },
   {
     title: 'mood',
     path: '/app/mood',
-    icon: <FontAwesomeIcon icon={faFaceLaughWink} />,
+    icon: <FontAwesomeIcon icon={faFaceLaughWink} size='lg' />,
   },
   {
     title: 'sleep',
     path: '/app/sleep',
-    icon: <FontAwesomeIcon icon={faBed} />,
+    icon: <FontAwesomeIcon icon={faBed} size='lg' />,
   },
   {
     title: 'activity',
     path: '/app/activity',
-    icon: <FontAwesomeIcon icon={faPersonRunning} />,
+    icon: <FontAwesomeIcon icon={faPersonRunning} size='lg' />,
   },
   {
     title: 'analysis',
     path: '/app/analysis',
-    icon: <FontAwesomeIcon icon={faMagnifyingGlassChart} />,
+    icon: <FontAwesomeIcon icon={faMagnifyingGlassChart} size='lg' />,
   },
 ];
 
