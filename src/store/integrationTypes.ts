c305f280a6b1c7060d3b0f865e6a017237bef463
Raaydon <billjrimell@gmail.com>
2024-02-29 17:59:49 +0000

    add sleepdata types

--- src/store/integrationTypes.ts
+++ src/store/integrationTypes.ts
@@ -6,7 +6,7 @@ export interface Integration {
   status: string;
 }
 
-export interface SleepData {
+export interface DailySleepData {
   id: string;
   contributors: {
       deep_sleep: number;
@@ -20,4 +20,56 @@ export interface SleepData {
   day: string;
   score: number;
   timestamp: string;
+}
+export interface SleepData {
+  id: string;
+  average_breath: number;
+  average_heart_rate: number;
+  average_hrv: number;
+  awake_time: number;
+  bedtime_end: string;
+  bedtime_start: string;
+  day: string;
+  deep_sleep_duration: number;
+  efficiency: number;
+  heart_rate: {
+    interval: number;
+    items: number[];
+    timestamp: string;
+  };
+  hrv: {
+    interval: number;
+    items: number[];
+    timestamp: string;
+  };
+  latency: number;
+  light_sleep_duration: number;
+  low_battery_alert: boolean;
+  lowest_heart_rate: number;
+  movement_30_sec: string;
+  period: number;
+  readiness: {
+    contributors: {
+      activity_balance: string;
+      body_temperature: string;
+      hrv_balance: string;
+      previous_day_activity: string;
+      previous_night: string;
+      recovery_index: string;
+      resting_heart_rate: string;
+      sleep_balance: string;
+    };
+    score: number;
+    temperature_deviation: number;
+    temperature_trend_deviation: number;
+  };
+  readiness_score_delta: number;
+  rem_sleep_duration: number;
+  restless_periods: number;
+  sleep_phase_5_min: string;
+  sleep_score_delta: number;
+  sleep_algorithm_version: string;
+  time_in_bed: number;
+  total_sleep_duration: number;
+  type: string;
 }
